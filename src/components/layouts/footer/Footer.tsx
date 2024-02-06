430e427eecc1729b6f5a7fe661c6b1ea68302028
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2024-02-07 00:30:09 +0100

    fix details on home and add footer

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -1,7 +1,9 @@
 import './footer.css';
 
 function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer>Conçu et réalisé par Gabriel Ivanes - Tous droits réservés</footer>
+	);
 }
 
 export default Footer;
